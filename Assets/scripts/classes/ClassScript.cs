4856bbace8eb6338604b44215a6a1c595db9c75d
Neko <nikotiini@protonmail.ch>
2017-12-20 19:01:37 +0200

    Class restructuring

--- Assets/scripts/classes/ClassScript.cs
+++ Assets/scripts/classes/ClassScript.cs
@@ -2,9 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 using UnityEngine.Networking;
 
-public abstract class ClassScript : NetworkBehaviour {
+public abstract class ClassScript : NetworkBehaviour
+{
 
+    protected PlayerScript player;
+
+    protected SpriteRenderer sprite;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponentInChildren<PlayerScript>();
+        sprite = player.gameObject.GetComponent<SpriteRenderer>();
+        ClassStart();
+    }
+
+    protected virtual void ClassStart()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.isLocalPlayer)
+        {
+            LocalPlayerUpdate();
+        }
+    }
+
+    protected abstract void LocalPlayerUpdate();
 }
 
